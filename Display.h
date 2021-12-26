b1a8ca8f28c4437255eaaa05fc03032670c4840a
mariamelsamni <96484561+mariamelsamni@users.noreply.github.com>
2021-12-26 14:48:18 +0200

    colours of players

--- Display.h
+++ Display.h
@@ -84,46 +84,86 @@ void difficulty_mode_ui(int p)
 }
 
 void twoplayersx3(int col,int row ,int player)
- {while(1)
-  {
-   char shape;
-   char A[6][10]={{32,'1','2','3','4','5'},
+ {
+char playerOne[12][2]={{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+char playerTwo[12][2]={{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+char A[6][10]={{32,'1','2','3','4','5'},
                   {'1',254,32,254,32,254},
                   {'2',32,32,32,32,32},
                   {'3',254,32,254,32,254},
                   {'4',32,32,32,32,32},
                   {'5',254,32,254,32,254}};
 
+int index1=0;
+int index2=0;
+while(1)
+
+  {
+    if (player%2==1)
+    {
+        playerOne[index1][0]=row;
+        playerOne[index1++][1]=col;
+    }
+    else
+    {
+        playerTwo[index2][0]=row;
+        playerTwo[index2++][1]=col;
+    }
+
+   for (int i=0;i<12;i++){
+    for (int j=0;j<2;j++){
+        printf("%d ",playerOne[i][j]);
+    }
+    printf("\n");
+   }
+   for (int i=0;i<12;i++){
+    for (int j=0;j<2;j++){
+        printf("%d ",playerTwo[i][j]);
+    }
+    printf("\n");
+   }
+   char shape;
+   player++;
    if (row%2==1 && row!=-1)
    {
         shape=205;
+        A[row][col]=shape;
    }
    else if (row%2==0 && row!=-1)
    {
         shape=186;
+        A[row][col]=shape;
    }
    printf("\n\n");
    for (int i=0;i<6;i++){
         printf("\t\t\t\t\t\t   ");
-    for(int j=0;j<10;j++){
-        if(i== row&& j==col)
+    for(int j=0;j<6;j++){
+        int variable =0;
+        for (int k=0;k<12;k++)
         {
-                if (player==1)
-                {
-                    color_char(RED,shape);
-                }
-                else if (player==2)
-                {
-                    color_char(BLUE,shape);
-                }
+            if (i==playerOne[k][0]&&j==playerOne[k][1])
+            {
+                color_char(RED,A[i][j]);
+                variable=1;
+                break;
+
+
+            }
+            else if (i==playerTwo[k][0]&&j==playerTwo[k][1])
+            {
+                color_char(BLUE,A[i][j]);
+                variable=1;
+                break;
+            }
 
         }
-        else{
-        printf("%c ",A[i][j]);
-        }
-    }
-    printf("\n");
-   }
+        if (variable==0)
+           {
+               printf("%c ",A[i][j]);
+           }
+
+
+   }printf("\n");}
 
    color_str(RED,"\n\tfirst player's name:","name");  color_str(BLUE,"\t\t\t\t\t   second player's name:","name");
    color_int(RED,"\n\n\tnumber of moves for first player:",5); color_int(BLUE,"\t\t\t\t   number of moves for second player:",5);
@@ -131,23 +171,22 @@ void twoplayersx3(int col,int row ,int player)
    color_int(YELLOW,"\n\n\tnumber of remaining lines:",3);
    color(CYAN,"\n\n\tenter 0,0 for undo\t1,1 for redo\t2,2 for save\t3,3 for main menu");
 
-   if (player==1)
+   if (player%2==1)
             printf("\n\n\t>>>>first player\'s turn:\n");
-   else if (player==2)
+   else if (player%2==0)
             printf("\n\n\t>>>>second player\'s turn:\n");
    color(RED,"\n\n\tENTER ROW:");
    printf(RED); scanf("%d",&row); printf(RESET);
    color(RED,"\tENTER COL:");
    printf(RED); scanf("%d",&col); printf(RESET);
-   // mmkn nstkhdem xor bs bgrb y3ny
+   // eletnen maynfa3osh yeb2o both even aw both odd
    while(row%2==0 && col%2==0 || row%2==1 && col%2==1)
    {
        //elmafrod hena 3yzen nclear akher talat sotor lesa mesh 3arfa ezay
        printf("\33[2K\r");
-       if (player==1)
-            printf("\n\n\t>>>>first player\'s turn:\n");
-       else if (player==2)
-            printf("\n\n\t>>>>second player\'s turn:\n");
+       printf("\n\tinvalid numbers \n");
+
+
        color(RED,"\n\n\tENTER ROW:");
        printf(RED); scanf("%d",&col); printf(RESET);
        color(RED,"\tENTER COL:");
