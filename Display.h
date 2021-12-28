a7844ce3cd265952d8143674f102b9418c352485
mariamelsamni <96484561+mariamelsamni@users.noreply.github.com>
2021-12-28 16:10:03 +0200

    redo last one

--- Display.h
+++ Display.h
@@ -75,7 +75,7 @@ void difficulty_mode_ui(int p)
         break;
     case 2:
         if (p == 3)
-            twoplayersx3();
+            twoplayersx3Names();
         else if(p == 2)
              break;
         break;
@@ -120,11 +120,15 @@ void getTime (int timeOfBeginning,int timehrsMinSec[])
      timehrsMinSec[2]=seconds;
 
 }
-
-void twoplayersx3()
+void twoplayersx3Names()
+{
+    char name1[10],name2[10];
+    GetNames(name1,name2);
+    twoplayersx3(name1,name2);
+}
+void twoplayersx3(char name1[],char name2[])
  {
-     char name1[10],name2[10];
-     GetNames(name1,name2);
+
 
 
 char playerOne[12][2]={{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
@@ -396,8 +400,19 @@ while(1)
 
    if (row==1&&col==1)
    {
+       if (turnsOfPlayer1==0 && turnsOfPlayer2==0)
+       {
            system("cls");
-           twoplayersx3();
+           twoplayersx3Names();
+
+       }
+       else
+       {
+           system("cls");
+           twoplayersx3(name1,name2);
+       }
+
+
 
    }
    if(row%2==0 && col%2==0 || row%2==1 && col%2==1|| row<0 || row>5||col<0||col>5)
